cab5f1b6d71b8a6af5f9c1501d42459d588081af
Augustus31 <supersumukh1@gmail.com>
2023-01-28 23:57:11 -0800

    gradients

---
+++ 2023_Gamejam/Assets/Scripts/gradientControllerUpper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gradientControllerUpper : MonoBehaviour
+{
+    public float skyHeight;
+    public GameObject skyLine;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skyLine = GameObject.Find("SkyLine");
+        skyHeight = skyLine.GetComponent<Transform>().position.y;
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        skyHeight = skyLine.GetComponent<Transform>().position.y;
+        if (skyHeight > 5.4)
+        {
+            skyHeight = 5.4f;
+        }
+        else if (skyHeight < -5.4)
+        {
+            skyHeight = -5.4f;
+        }
+        transform.localPosition = new Vector3(0, (-5.4f - skyHeight) * -50f, 0);
+        image.fillAmount = (10.8f - (skyHeight + 5.4f) + ((5.4f - -1*skyHeight) / 2)) / 10.8f;
+    }
+}
