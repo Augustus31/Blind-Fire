c8cdb3e9d64ff386aac125005291845af7ae698d
Augustus31 <supersumukh1@gmail.com>
2023-01-29 18:17:32 -0800

    lives

--- 2023_Gamejam/Assets/Scripts/PlayerController.cs
+++ 2023_Gamejam/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public GameController gameController;
+    public int lives;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lives = 3;
+        heart1 = GameObject.Find("heart1");
+        heart2 = GameObject.Find("heart2");
+        heart3 = GameObject.Find("heart3");
     }
 
     // Update is called once per frame
@@ -21,9 +30,35 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag == "obstacle")
         {
-            Debug.Log("Collision detected!");
-            // TODO: only trigger when life is depleted
-            gameController.gameOver();
+            
+            try
+            {
+                collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            catch
+            {
+                collision.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            }
+            if (lives > 1)
+            {
+                if(lives == 3)
+                {
+                    heart1.GetComponent<Image>().enabled = false;
+                }
+                else if (lives == 2)
+                {
+                    heart2.GetComponent<Image>().enabled = false;
+                }
+                lives--;
+            }
+            else
+            {
+                heart3.GetComponent<Image>().enabled = false;
+                lives = 0;
+                Debug.Log("Collision detected!");
+                // TODO: only trigger when life is depleted
+                gameController.gameOver();
+            }  
         }
     }
 }
