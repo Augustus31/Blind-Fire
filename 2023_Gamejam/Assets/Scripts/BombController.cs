9c872acff9b2b843fb477aac5fa2fb28df589040
Momopey <m22461@gmail.com>
2023-01-29 21:55:22 -0800

    Bomb sfx

--- 2023_Gamejam/Assets/Scripts/BombController.cs
+++ 2023_Gamejam/Assets/Scripts/BombController.cs
@@ -8,6 +8,11 @@ public class BombController : MonoBehaviour
     public float explodeSpeed;
     public float explodeScale = 2;
 
+    public float fuzeVolume = 0.5f;
+    public AudioClip fuzeSfx;
+    public AudioClip explodeSfx;
+
+
     private Vector3 explodeLocation;
     private Vector3 startingLocation;
     private Vector3 moveDirection;
@@ -15,6 +20,9 @@ public class BombController : MonoBehaviour
     private float tExplode = 0;
 
     private Animator bombAnimator;
+
+    private AudioSource fuzeSource;
+    private AudioSource explodeSource; 
 
     // Start is called before the first frame update
     void Start()
@@ -30,13 +38,40 @@ public class BombController : MonoBehaviour
         this.transform.position = startingLocation;
 
         bombAnimator = GetComponent<Animator>();
+
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        if(audioSources.Length >= 2)
+        {
+            fuzeSource = audioSources[0];
+            fuzeSource.loop = true;
+            fuzeSource.clip = fuzeSfx;
+            fuzeSource.volume = fuzeVolume;
+
+            explodeSource = audioSources[1];
+            explodeSource.clip = explodeSfx;
+        }
+        else
+        {
+            Debug.LogError("Bomb is missing audio sources");
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(fuzeSource != null && !fuzeSource.isPlaying)
+        {
+            //Start the fuze sound on spawn
+            fuzeSource.Play();
+        }
         Vector3 newMoveVector = moveDirection * moveSpeed * Time.deltaTime;
         if ((explodeLocation - this.transform.position).magnitude < newMoveVector.magnitude) {
+            // Explosion -> turn off fuze sound, play explode sound
+            if(tExplode == 0) { 
+                fuzeSource.Stop();
+                explodeSource.Play();
+            }
+
             this.transform.position = explodeLocation;
             // Not sure if using scale is the best way to simulate explosion
             Debug.Log("EXPLODE");
