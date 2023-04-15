2a7ed55b2a65923df2e8dbc6cd790a395ce7cb53
Augustus31 <supersumukh1@gmail.com>
2023-04-14 20:05:51 -0700

    cleared file

--- 2023_Gamejam/Assets/Scripts/ObstacleAttributes.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObstacleAttributes : MonoBehaviour
-{
-    public float timeToSpawn = 1;
-    public float chanceToSpawn = 0.5f;
-}
