95cf1e093a0163320a8423064bd6069e407869d3
Kiichi <darkaces272@gmail.com>
2023-04-15 12:46:55 -0700

    Added Menu Scripts and Scene

--- SpringGameJam/Assets/Scripts/Menu.cs
+++ SpringGameJam/Assets/Scripts/Menu.cs
@@ -5,18 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Quit()
     {
-
+        Application.Quit();
     }
 
     public void NextLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
